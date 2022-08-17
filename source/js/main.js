d4914cf52ca0be7052dad0f7a0e69c33d94cc529
kozyrek <kozyrevaia@bk.ru>
2022-08-17 13:04:59 +0500

    подготавливает блоки с преимуществами и о компании

--- source/js/main.js
+++ source/js/main.js
@@ -2,6 +2,7 @@
 // import {initModals} from './modules/modals/init-modals';
 
 import {openAccordion} from './modules/open-accordion';
+import {openDetails} from './modules/open-details';
 
 // ---------------------------------
 
@@ -19,9 +20,10 @@ window.addEventListener('DOMContentLoaded', () => {
 
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
-  // window.addEventListener('load', () => {
-  //   // initModals();
-  // });
+  window.addEventListener('load', () => {
+    // initModals();
+    openDetails();
+  });
 });
 
 // ---------------------------------
---
+++ source/js/modules/open-details.js
@@ -0,0 +1,16 @@
+const button = document.querySelector('.description__button');
+const descriptionText = document.querySelector('.description');
+
+const openDetails = () => {
+  button.addEventListener('click', () => {
+    descriptionText.classList.toggle('description--open-details');
+
+    if (descriptionText.classList.contains('description--open-details')) {
+      button.textContent = 'Свернуть';
+    } else {
+      button.textContent = 'Подробнее';
+    }
+  });
+};
+
+export {openDetails};
