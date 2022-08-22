0a7c65d262f2f4d8022b2ae5c11eee1adc62d8ed
kozyrek <kozyrevaia@bk.ru>
2022-08-22 23:06:11 +0500

    правит замечания

--- source/js/modules/open-details.js
+++ source/js/modules/open-details.js
@@ -2,15 +2,17 @@ const button = document.querySelector('.description__button');
 const descriptionText = document.querySelector('.description');
 
 const openDetails = () => {
-  button.addEventListener('click', () => {
-    descriptionText.classList.toggle('description--open-details');
+  if (button) {
+    button.addEventListener('click', () => {
+      descriptionText.classList.toggle('description--open-details');
 
-    if (descriptionText.classList.contains('description--open-details')) {
-      button.textContent = 'Свернуть';
-    } else {
-      button.textContent = 'Подробнее';
-    }
-  });
+      if (descriptionText.classList.contains('description--open-details')) {
+        button.textContent = 'Свернуть';
+      } else {
+        button.textContent = 'Подробнее';
+      }
+    });
+  }
 };
 
 export {openDetails};
--- source/js/modules/open-modal.js
+++ source/js/modules/open-modal.js
@@ -1,16 +1,10 @@
 const page = document.querySelector('.page-body');
 const modal = document.querySelector('.modal');
 const buttonOpen = document.querySelector('.page-header__button');
-const buttonClose = modal.querySelector('.modal__close-button');
-const modalForm = modal.querySelector('.feedback-form--modal');
+const buttonClose = document.querySelector('.modal__close-button');
+const modalForm = document.querySelector('.feedback-form--modal');
 const focusableElementsString = 'button:not([disabled]), input, textarea';
-const focusableElements = modal.querySelectorAll(focusableElementsString);
 const focusInput = document.getElementById('first-name-modal');
-
-const modalElements = Array.from(focusableElements);
-const firstTabStop = modalElements[0];
-const lastTabStop = modalElements[modalElements.length - 1];
-
 
 const existVerticalScroll = () => {
   return document.body.offsetHeight > window.innerHeight;
@@ -20,81 +14,94 @@ const getBodyScrollTop = () => {
   return self.pageYOffset || (document.documentElement && document.documentElement.ScrollTop) || (document.body && document.body.scrollTop);
 };
 
-
-const setClass = () => {
-  modal.classList.toggle('modal--close');
-  page.classList.toggle('page-body__lock');
+const setClass = (item) => {
+  if (item) {
+    item.classList.toggle('modal--close');
+    page.classList.toggle('page-body__lock');
+  }
 };
 
 const openModal = () => {
-  buttonOpen.addEventListener('click', function (evt) {
-    evt.preventDefault();
+  if (buttonOpen) {
+    buttonOpen.addEventListener('click', function (evt) {
+      evt.preventDefault();
 
-    page.dataset.scrollY = getBodyScrollTop();
+      page.dataset.scrollY = getBodyScrollTop();
 
-    setClass();
-    focusInput.focus();
+      setClass(modal);
+      if (focusInput) {
+        focusInput.focus();
+      }
 
-    if (existVerticalScroll()) {
-      page.style.top = `-${page.dataset.scrollY}px`;
-    }
-  });
+      if (existVerticalScroll()) {
+        page.style.top = `-${page.dataset.scrollY}px`;
+      }
+    });
+  }
 
-  modal.addEventListener('keydown', function (evt) {
-    if (evt.keyCode === 9) {
-      if (evt.shiftKey) {
-        if (document.activeElement === firstTabStop) {
-          evt.preventDefault();
-          lastTabStop.focus();
-        }
-      } else {
-        if (document.activeElement === lastTabStop) {
-          evt.preventDefault();
-          firstTabStop.focus();
+  if (modal) {
+    const focusableElements = modal.querySelectorAll(focusableElementsString);
+    const modalElements = Array.from(focusableElements);
+    const firstTabStop = modalElements[0];
+    const lastTabStop = modalElements[modalElements.length - 1];
+
+    modal.addEventListener('keydown', function (evt) {
+      if (evt.keyCode === 9) {
+        if (evt.shiftKey) {
+          if (document.activeElement === firstTabStop) {
+            evt.preventDefault();
+            lastTabStop.focus();
+          }
+        } else {
+          if (document.activeElement === lastTabStop) {
+            evt.preventDefault();
+            firstTabStop.focus();
+          }
         }
       }
-    }
-  });
-
+    });
+  }
 };
 
 const closeModal = () => {
-  buttonClose.addEventListener('click', function (evt) {
-    evt.preventDefault();
-    setClass();
-    buttonOpen.focus();
-
-    if (existVerticalScroll()) {
-      window.scrollTo(0, page.dataset.scrollY);
-    }
-  });
-
-  modal.addEventListener('click', function (evt) {
-    const target = evt.target;
-    const itsModalForm = target === modalForm || modalForm.contains(target);
-    if (!itsModalForm) {
-      setClass();
-      buttonOpen.focus();
-
-      if (existVerticalScroll()) {
-        window.scrollTo(0, page.dataset.scrollY);
-      }
-    }
-  });
-
-  modal.addEventListener('keydown', function (evt) {
-    if (evt.keyCode === 27) {
+  if (buttonClose) {
+    buttonClose.addEventListener('click', function (evt) {
       evt.preventDefault();
-      setClass();
+      setClass(modal);
       buttonOpen.focus();
 
       if (existVerticalScroll()) {
         window.scrollTo(0, page.dataset.scrollY);
       }
-    }
-  });
+    });
+  }
 
+  if (modal) {
+    modal.addEventListener('click', function (evt) {
+      const target = evt.target;
+      const itsModalForm = target === modalForm || modalForm.contains(target);
+      if (!itsModalForm) {
+        setClass(modal);
+        buttonOpen.focus();
 
+        if (existVerticalScroll()) {
+          window.scrollTo(0, page.dataset.scrollY);
+        }
+      }
+    });
+
+    modal.addEventListener('keydown', function (evt) {
+      if (evt.keyCode === 27) {
+        evt.preventDefault();
+        setClass(modal);
+        buttonOpen.focus();
+
+        if (existVerticalScroll()) {
+          window.scrollTo(0, page.dataset.scrollY);
+        }
+      }
+    });
+  }
 };
 
 export {openModal, closeModal};
